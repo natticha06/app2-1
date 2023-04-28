48b2669f211a73e5ae03e69bdfdcec84f094d75b
natticha06 <265345221008-st@rmutsb.ac.th>
2023-04-28 11:43:06 +0700

    first commit 265345221008

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react'
-import {MyAout,MyImage,MyData} from './func-mydata'
-import {Navbar,Header,Footer} from './func-components'
-import Calendar from './class-components'
-import MsgBox from './func-props'
-
-
+//import {MyAbout,MyImage,MyData} from './func-mydata'
+//import {Navbar,Header,Footer}  from './func-components'
+//import Calendar from './class-components'
+//import MsgBox from './func-props'
+//import MsgBox2 from './class-props'
+import List from './list-comp'
 
 function App() {
+  return (<List/>)
 
-return [<Navbar/>,<Calendar />,<Header />,<Footer/>]
-/*return(
-  <MsgBox 
-  text="Useing props in function component"
-  colr="red"
-  bgColor="#ccc"
-  fontSize="16pt"
-  border="dotted 1px black"
-  />
-)*/
+//return [<Navbar />,<Calendar />,<Header />,<Footer/>]
+
+  /*return(
+      <MsgBox2
+      text="Useing props in function component"
+      color="green"
+      bgColor="#ccc"
+      fontSize="16px"
+      border="dotted 1px black"
+      />
+  )*/
 }
-
 export default App;
-
